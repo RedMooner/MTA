f4b30513dc81a4a3932e2df0ff3749990381c51d
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2025-02-20 15:26:15 +0500

    Update 0.1.3/2: Change a shortcut to hide/open app

--- jsapp/main.js
+++ jsapp/main.js
@@ -4,20 +4,9 @@ const { nativeImage } = require('electron');
 const extractIcon = require('./core/icon_extractor');
 
 let mainWindow;
-
-
-
+let currentShortcut = 'CommandOrControl+X';
 
 function createWindow() {
-
-
-
-
-
-
-
-
-    
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -43,23 +32,36 @@ function createWindow() {
         mainWindow = null;
     });
 
-
-
-
+    // Добавляем обводку и полупрозрачность
+    mainWindow.setBackgroundColor('#1e1e1e80'); // Полупрозрачный цвет фона
+    mainWindow.setOpacity(0.9); // Полупрозрачность окна
 }
 
-
-ipcMain.on("open-file",function(event,path){
+ipcMain.on("open-file", function (event, path) {
     const { exec } = require('child_process');
-    exec(path)
-    mainWindow.hide()
-})
+    exec(path);
+    mainWindow.hide();
+});
+
+ipcMain.on('update-shortcut', (event, newShortcut) => {
+    globalShortcut.unregisterAll();
+    globalShortcut.register(newShortcut, () => {
+        if (mainWindow) {
+            if (mainWindow.isVisible()) {
+                mainWindow.hide();
+            } else {
+                mainWindow.show();
+            }
+        }
+    });
+    currentShortcut = newShortcut;
+});
 
 app.on('ready', () => {
     createWindow();
 
-    // Регистрируем глобальный шорткат Ctrl + Alt
-    globalShortcut.register('A', () => {
+    // Регистрируем глобальный шорткат
+    globalShortcut.register(currentShortcut, () => {
         if (mainWindow) {
             if (mainWindow.isVisible()) {
                 mainWindow.hide();
@@ -69,7 +71,6 @@ app.on('ready', () => {
         }
     });
 });
-
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit();
